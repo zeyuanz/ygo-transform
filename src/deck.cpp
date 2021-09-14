5d6a0a8b8432ad926b02a40c273d0b3b32ac7c27
zeyuanz <zeyuanz@zeyuanzdeMacBook-Pro.local>
2021-09-14 17:49:38 +0800

    done

--- src/deck.cpp
+++ src/deck.cpp
@@ -57,6 +57,7 @@ std::string Deck::get_cards(std::vector<Card> m, std::vector<Card> e, std::vecto
 
 		o_deck += side;
 		create_deck_pro2(o_deck, s);	
+		o_deck = o_deck.substr(0, o_deck.size()-1); // remove final '\n'
 	} else if (format == "pro2") {
 		o_deck = "ygo://deck";
 		o_deck += main;
@@ -135,34 +136,36 @@ void construct_card_deck_pro2(std::vector<std::string> content_splits, int start
 
 void parse_mobile_deck(std::string content, std::vector<Card> &m, std::vector<Card> &e, std::vector<Card> &s) {
 	int start = content.find("main=") + 5;
-	int end = content.find("extra=");
+	int end = content.find("&extra=");
 	construct_card_deck_mobile(content.substr(start, end-start), m);
 
-	start = end+6;
-	end = content.find("side=");
+	start = end+7;
+	end = content.find("&side=");
 	construct_card_deck_mobile(content.substr(start, end-start), e);
 
-	start = end+5;
+	start = end+6;
 	construct_card_deck_mobile(content.substr(start), s);
 }
 
 void construct_card_deck_mobile(std::string content, std::vector<Card> &c) {
 	int start = 0, end = 0;
-	while (1) {
+	bool continue_flag = true;
+	while (continue_flag) {
 		end = content.find('_', start);
 		if (end == -1) {
-			break;
+			continue_flag = false;
+			end = content.size();
 		}
 		std::string card_tmp = content.substr(start, end-start);
 		int times;
 		if (card_tmp.find('*') == -1) { 
 			times = 1; 
 		} else { 
+			if (card_tmp.find('\n') != -1) {card_tmp = card_tmp.substr(0, card_tmp.size()-1);}
 			times = card_tmp[card_tmp.size()-1] - '0'; 
 			card_tmp = card_tmp.substr(0, card_tmp.size()-2);
 		}
 		c.push_back(Card(card_tmp, times));
-
 		end++;
 		start = end;
 	}
